bb429e52158cdfee82eb306949bb39e7583eff3e
Alan <71236097+AlanAngeli@users.noreply.github.com>
2022-02-08 11:49:53 -0300

    Add files via upload

---
+++ condicional_or.py
@@ -0,0 +1,12 @@
+nome = input ("Qual seu nome? ")
+
+if nome :
+    print(nome)
+else:
+    print("Você não digitou nada")
+
+#####################################
+
+nome = input("Digite seu nome: ")
+
+print(nome or "Você não digitou nada") # forma simplificada
